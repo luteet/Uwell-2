d56915773a29d59d9148165f79c0b74874442f6c
Roman Lukjanets <lukyanets14@gmail.com>
2023-06-07 14:57:26 +0300

    💊

--- dist/js/main.js
+++ dist/js/main.js
@@ -143,12 +143,165 @@ document.addEventListener('scroll', scroll);
 
 const doctorCardVideoWrapper = document.querySelectorAll('.doctor-card__video--wrapper');
 doctorCardVideoWrapper.forEach(doctorCardVideoWrapper => {
-	const iframe = doctorCardVideoWrapper.querySelector('iframe');
-	if(iframe) {
-		if(iframe.getAttribute('width') && iframe.getAttribute('height')) {
-			doctorCardVideoWrapper.style.setProperty('--aspect-ratio', Number(iframe.getAttribute('height')) / Number(iframe.getAttribute('width')) * 100 + '%');
+	const video = doctorCardVideoWrapper.querySelector('video');
+	if(video) {
+		if(video.getAttribute('width') && video.getAttribute('height')) {
+			doctorCardVideoWrapper.style.setProperty('--aspect-ratio', Number(video.getAttribute('height')) / Number(video.getAttribute('width')) * 100 + '%');
 		}
 	}
 })
 
+function Popup(arg) {
+
+	let body = document.querySelector('body'),
+		html = document.querySelector('html'),
+		saveHref = (typeof arg == 'object') ? (arg['saveHref']) ? true : false : false,
+		popupCheck = true,
+		popupCheckClose = true;
+
+	const removeHash = function () {
+		let uri = window.location.toString();
+		if (uri.indexOf("#") > 0) {
+			let clean_uri = uri.substring(0, uri.indexOf("#"));
+			window.history.replaceState({}, document.title, clean_uri);
+		}
+	}
+
+	const open = function (id, initStart) {
+
+		if (popupCheck) {
+			popupCheck = false;
+
+			let popup = document.querySelector(id);
+
+			if (popup) {
+
+				body.classList.remove('_popup-active');
+				html.style.setProperty('--popup-padding', window.innerWidth - body.offsetWidth + 'px');
+				body.classList.add('_popup-active');
+
+				if (saveHref) history.pushState('', "", id);
+
+				setTimeout(() => {
+					if (!initStart) {
+						popup.classList.add('_active');
+						function openFunc() {
+							popupCheck = true;
+							popup.removeEventListener('transitionend', openFunc);
+						}
+						popup.addEventListener('transitionend', openFunc)
+					} else {
+						popup.classList.add('_transition-none');
+						popup.classList.add('_active');
+						popupCheck = true;
+					}
+
+				}, 0)
+
+			} else {
+				return new Error(`Not found "${id}"`)
+			}
+		}
+	}
+
+	const close = function (popupClose) {
+		if (popupCheckClose) {
+			popupCheckClose = false;
+
+			let popup
+			if (typeof popupClose === 'string') {
+				popup = document.querySelector(popupClose)
+			} else {
+				popup = popupClose.closest('.popup');
+			}
+
+			if (popup.classList.contains('_transition-none')) popup.classList.remove('_transition-none')
+
+			setTimeout(() => {
+				popup.classList.remove('_active');
+				function closeFunc() {
+					const activePopups = document.querySelectorAll('.popup._active');
+
+					if (activePopups.length < 1) {
+						body.classList.remove('_popup-active');
+						html.style.setProperty('--popup-padding', '0px');
+					}
+
+					if (saveHref) {
+						removeHash();
+						if (activePopups[activePopups.length - 1]) {
+							history.pushState('', "", "#" + activePopups[activePopups.length - 1].getAttribute('id'));
+						}
+					}
+
+					popupCheckClose = true;
+					popup.removeEventListener('transitionend', closeFunc)
+				}
+
+				popup.addEventListener('transitionend', closeFunc)
+
+			}, 0)
+
+		}
+	}
+
+	return {
+
+		open: function (id) {
+			open(id);
+		},
+
+		close: function (popupClose) {
+			close(popupClose)
+		},
+
+		init: function () {
+
+			let thisTarget
+			body.addEventListener('click', function (event) {
+
+				thisTarget = event.target;
+
+				let popupOpen = thisTarget.closest('.open-popup');
+				if (popupOpen) {
+					event.preventDefault();
+					open(popupOpen.getAttribute('href'))
+				}
+
+				let popupClose = thisTarget.closest('.popup-close');
+				if (popupClose) {
+					close(popupClose);
+					const popup = thisTarget.closest('.popup');
+					if(popup.dataset.timer) localStorage.setItem("uwell-2-application-popup", true);
+					
+				}
+
+			});
+
+			const popups = document.querySelectorAll('.popup');
+			popups.forEach(popup => {
+				let timer = popup.dataset.timer;
+				if(timer && localStorage.getItem("uwell-2-application-popup") != "true") {
+					if(Number(timer)) timer = Number(timer) * 1000;
+					
+					setTimeout(() => {
+						open("#" + popup.getAttribute('id'));
+					}, timer)
+				}
+			})
+
+			if (saveHref) {
+				let url = new URL(window.location);
+				if (url.hash) {
+					open(url.hash, true);
+				}
+			}
+		},
+
+	}
+}
+
+const popup = new Popup();
+
+popup.init()
 
